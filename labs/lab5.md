a29aa994c49c1f80a3193b4694eaa6673afd26ac
Gary Lai <garylai1990@gmail.com>
2018-06-04 21:38:55 -0400

    lab5

---
+++ labs/lab5.md
@@ -0,0 +1,22 @@
+# Lab 5
+
+## Goals:
+
+- Learn to write for-loops to make text based drawings.
+
+Please, make sure that you have added the honor code statement at the top of your Java file:
+
+```java
+/*
+  THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING       
+  CODE WRITTEN BY OTHER STUDENTS. _Your_Name_Here_
+*/
+```
+
+## Preparation: Create your ~/cs170/lab5/, and copy files
+
+```
+mkdir ~/cs170/lab5
+cp  ~cs170001/share/lab5/*  ~/cs170/lab5
+cd ~/cs170/lab5
+```
--- labs/lab5/Sample.java
+++ labs/lab5/Sample.java
@@ -1,56 +1,49 @@
-public class Sample
-{
-   public static void main(String[] args) 
-   {
-      int i;
+public class Sample {
+  public static void main(String[] args) {
+    int i;
 
-      /* -----------------------------------------
-         Make sure user provides 1 argument
-         ----------------------------------------- */
-      if ( args.length != 1 )
-      {
-         System.err.println("Error: Program needs 1 integer as input");
-	 System.exit(1);
-      }
+    /* -----------------------------------------
+       Make sure user provides 1 argument
+       ----------------------------------------- */
+    if (args.length != 1) {
+      System.err.println("Error: Program needs 1 integer as input");
+      System.exit(1);
+    }
 
-      /* -----------------------------------------
-         Convert first argument to integer
-         ----------------------------------------- */
-      int n = Integer.parseInt( args[0] );
+    /* -----------------------------------------
+       Convert first argument to integer
+       ----------------------------------------- */
+    int n = Integer.parseInt(args[0]);
 
-      /* -----------------------------------------------
-         Use indentCharLine() to print a nxn square
+    /* -----------------------------------------------
+       Use indentCharLine() to print a nxn square
 
-         Print n rows of character 'X' of width n
-         ----------------------------------------------- */
-      for ( i = 0; i < n; i++ )
-      {
-         indentCharLine(1, 'X', n);
-      }
-   }
-   
-   
-   /* ---------------------------------------------------------------
-      repeatChar(c,n): Prints the character c, n times.
-      --------------------------------------------------------------- */
-   public static void repeatChar(char c, int n)
-   {
-      for(int j=0; j<n; j++)
-         System.out.print(c);   
-   }
-   
-   /* ----------------------------------------------------------------
-      indentCharLine(i, c, n):
+       Print n rows of character 'X' of width n
+       ----------------------------------------------- */
+    for (i = 0; i < n; i++) {
+      indentCharLine(1, 'X', n);
+    }
+  }
 
-          First, it prints i spaces (i.e., indents i positions)
-          Then, it prints the character c, n times.
-          Finally, it prints NEWLINE (to start a new line)
-      ---------------------------------------------------------------- */
-   public static void indentCharLine(int i, char c, int n)
-   {
-      repeatChar(' ',i);
-      repeatChar(c,n);
-      repeatChar('\n',1);
-   }
-   
+  /* ---------------------------------------------------------------
+     repeatChar(c,n): Prints the character c, n times.
+     --------------------------------------------------------------- */
+  public static void repeatChar(char c, int n) {
+    for (int j = 0; j < n; j++) {
+      System.out.print(c);
+    }
+  }
+
+  /* ----------------------------------------------------------------
+     indentCharLine(i, c, n):
+
+         First, it prints i spaces (i.e., indents i positions)
+         Then, it prints the character c, n times.
+         Finally, it prints NEWLINE (to start a new line)
+     ---------------------------------------------------------------- */
+  public static void indentCharLine(int i, char c, int n) {
+    repeatChar(' ', i);
+    repeatChar(c, n);
+    repeatChar('\n', 1);
+  }
 }
